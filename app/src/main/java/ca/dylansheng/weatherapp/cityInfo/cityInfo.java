c58c2d4aac722da8d26460fd7695416d859b7c27
dylansheng <shengdiansd@gmail.com>
2016-10-19 15:07:42 -0600

    ..

---
+++ app/src/main/java/ca/dylansheng/weatherapp/cityInfo/cityInfo.java
@@ -0,0 +1,12 @@
+package ca.dylansheng.weatherapp.cityInfo;
+
+/**
+ * Created by sheng on 2016/10/19.
+ */
+
+public class cityInfo {
+    private String cityName;
+    private Double latitude;
+    private Double longitude;
+    private Integer temperature;
+}
