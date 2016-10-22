1ecdb28ced7fd81937a3ba0fbccd0f085a7497ef
dylansheng <shengdiansd@gmail.com>
2016-10-22 09:25:19 -0600

    remove action bar

--- app/src/main/java/ca/dylansheng/weatherapp/activity/MainActivity.java
+++ app/src/main/java/ca/dylansheng/weatherapp/activity/MainActivity.java
@@ -4,11 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import ca.dylansheng.weatherapp.R;
 
-public class MainActivity extends Activity {
+import static android.R.attr.onClick;
+
+public class MainActivity extends Activity implements View.OnTouchListener{
+    private ImageButton imageButtonMainActivity_1;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +26,39 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
+        gestureDetector = new GestureDetector(this, new SingleTapConfirm());
+        imageButtonMainActivity_1 = (ImageButton) findViewById(R.id.imageButtonMainActivity_1) ;
+        imageButtonMainActivity_1.setOnTouchListener(this);
+
         //intent to getInfoActivity
-        Intent intent = new Intent(MainActivity.this, getInfoActivity.class);
-        startActivity(intent);
+
     }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (v.getId()){
+            case R.id.imageButtonMainActivity_1:
+                if (gestureDetector.onTouchEvent(event)) {
+                    Intent intent = new Intent(MainActivity.this, getInfoActivity.class);
+                    startActivity(intent);
+                } else {
+                    // your code for move and drag
+                }
+
+                break;
+            default:
+                break;
+        }
+        return false;
+    }
+
+    private class SingleTapConfirm extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent event) {
+            return true;
+        }
+    }
+
 }
