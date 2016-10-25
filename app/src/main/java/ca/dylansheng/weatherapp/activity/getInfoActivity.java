f0ba0aadf56cb37f54c74cced34ddfc59c4d0f15
dylansheng <shengdiansd@gmail.com>
2016-10-25 14:02:28 -0600

    update swipe layout

--- app/src/main/java/ca/dylansheng/weatherapp/activity/getInfoActivity.java
+++ app/src/main/java/ca/dylansheng/weatherapp/activity/getInfoActivity.java
@@ -18,6 +18,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.daimajia.swipe.SwipeLayout;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -63,7 +65,47 @@ public class getInfoActivity extends Activity implements View.OnClickListener{
         imageViewCityImage = (ImageView) findViewById(R.id.imageViewCityImage);
         dbHelper  = new MyDatabaseHelper(getInfoActivity.this,"weatherDB.db",null,1);
 
+        SwipeLayout swipeLayout =  (SwipeLayout)findViewById(R.id.sample1);
 
+        //set show mode.
+        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+
+//add drag edge.(If the BottomView has 'layout_gravity' attribute, this line is unnecessary)
+        swipeLayout.addDrag(SwipeLayout.DragEdge.Left, findViewById(R.id.bottom_wrapper));
+
+        swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                //when the BottomView totally show.
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+            }
+        });
+
+        
 /*
         if(city.cityName == null){
             city.cityName = "edmonton";
